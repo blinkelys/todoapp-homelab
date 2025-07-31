048d3daa87d66a8abe3b754123253c6b0d6b5f5e
blinkelys <martinustomrenslemmen@gmail.com>
2025-07-31 22:06:46 +0200

    Feat: top bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,14 @@
 <template>
+  <TopBar />
   <router-view />
 </template>
+
+<script>
+  import TopBar from './components/layout/topBar.vue'
+
+  export default {
+    components: {
+      TopBar
+    }
+  }
+</script>
---
+++ client/src/components/layout/topBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="w-full h-fit py-5 bg-gray-950 text-white flex justify-between items-center px-20">
+        <h1 class="text-2xl font-light">Welcome, User</h1>
+        <p class="text-2xl font-light">{{ new Date().toLocaleTimeString() }}</p>
+        <p class="text-2xl font-light">{{ new Date().toLocaleDateString() }}</p>
+    </div>
+</template>
